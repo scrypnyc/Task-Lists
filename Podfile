2041dcb6b83c048e9efa2a42457beb93205703e1
scrypnyc <alexanderlookua@gmail.com>
2019-04-30 17:46:46 +0300

    Commit - 1

--- Podfile
+++ Podfile
@@ -1,11 +1,15 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
 
 target 'Task-Lists-for-Realm' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
+  pod 'RealmSwift'
+  pod 'Realm'
 
   # Pods for Task-Lists-for-Realm
-  pod 'RealmSwift'
+
+
 
 end
--- Task-Lists-for-Realm/ViewController.swift
+++ Task-Lists-for-Realm/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+  
+  // - контейнер со свойствами объекта TaskList
+  var items = [String]()
+  // - идентификатор ячейки
+  var cellId = "Cell"
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    // - цвет view
+    view.backgroundColor = .brown
+    // - цвет navigation bar
+    navigationController?.navigationBar.barTintColor = .white
+    // - цвет текста для кнопки
+    navigationController?.navigationBar.tintColor = .white
+    
+    // - конопка "добавить" n-bar
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add",
+                                                        style: .plain,
+                                                        target: self,
+                                                        action: #selector(addItem)) // - вызов метода для кнопки
+    
+    // - ячейка для TableView c индификатором "Cell"
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+  }
+  // - Действие при нажатии на кнопку "Add"
+  @objc func addItem(_ sender: AnyObject) {
+    
+    addAlertForNewItem()
   }
 
+  func addAlertForNewItem() {
+    
+    // - alert
+    let alert = UIAlertController(title: "New Task", message: "Please enter text", preferredStyle: .alert)
+    // - создание текстового поля
+    var alertTextField: UITextField!
+    alert.addTextField { TextField in
+      alertTextField = TextField
+      TextField.placeholder = "New Task"
+  }
+
+    // - создание кнопки для сохранения новых значений
+    let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+      // проверяем не является ли поле пустым
+      guard let text = alertTextField.text, !text.isEmpty else { return }
+      // добавление новоего элемента в массив задач
+      self.items.append(text)
+      // обновение таблицы
+    }
 
 }
 
+}

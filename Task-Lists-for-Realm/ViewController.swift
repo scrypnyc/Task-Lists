e9790833786d2a692cf63d6c0338dfc66fc58134
scrypnyc <alexanderlookua@gmail.com>
2019-04-30 13:25:05 +0300

    Initial Commit

---
+++ Task-Lists-for-Realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task-Lists-for-Realm
+//
+//  Created by Alexander Skrypnyk on 4/30/19.
+//  Copyright © 2019 skrypnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
